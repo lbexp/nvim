42bb8aeca311c3149606819cb9e824addfae5dc3
lbexp <fiqarapr@gmail.com>
2024-09-28 21:47:57 +0800

    feat: add wakatime

--- lua/lbexp/packer.lua
+++ lua/lbexp/packer.lua
@@ -32,4 +32,6 @@ return require('packer').startup(function(use)
   use({'neovim/nvim-lspconfig'})
   use({'hrsh7th/nvim-cmp'})
   use({'hrsh7th/cmp-nvim-lsp'})
+
+  use 'wakatime/vim-wakatime'
 end)
