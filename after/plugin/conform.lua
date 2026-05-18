19d4cf3e5eaa1f669b95f5c023c9cb13471eed54
Fiqar Aprialim <fiqarapr@gmail.com>
2026-05-18 22:30:03 +0700

    style: format with new lua formatter

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,34 +1,35 @@
 local conform = require("conform")
 
 conform.setup({
-    formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        cpp = { "clang_format" },
-        c = { "clang_format" }
-    },
-    formatters = {
-        clang_format = {
-            args = '--style="{IndentWidth: 4, AllowShortFunctionsOnASingleLine: Empty}"'
-        },
-        prettier = {
-            prepend_args = {
-                '--use-space',
-                '--space-width 2',
-                '--single-quote'
-            }
-        }
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 1000,
-        lsp_format = "fallback",
-    },
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		cpp = { "clang_format" },
+		c = { "clang_format" },
+		lua = { "stylua" },
+	},
+	formatters = {
+		clang_format = {
+			args = '--style="{IndentWidth: 4, AllowShortFunctionsOnASingleLine: Empty}"',
+		},
+		prettier = {
+			prepend_args = {
+				"--use-space",
+				"--space-width 2",
+				"--single-quote",
+			},
+		},
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 1000,
+		lsp_format = "fallback",
+	},
 })
--- lua/lbexp/packer.lua
+++ lua/lbexp/packer.lua
@@ -1,123 +1,124 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Nvim package manager
-    use {
-        'wbthomason/packer.nvim'
-    }
+return require("packer").startup(function(use)
+	-- Nvim package manager
+	use({
+		"wbthomason/packer.nvim",
+	})
 
-    -- Tooling package manager
-    use {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    }
+	-- Tooling package manager
+	use({
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	})
 
-    -- Editor theme
-    use {
-        'craftzdog/solarized-osaka.nvim',
-        config = function()
-            vim.cmd.colorscheme 'solarized-osaka'
-        end
-    }
+	-- Editor theme
+	use({
+		"craftzdog/solarized-osaka.nvim",
+		config = function()
+			vim.cmd.colorscheme("solarized-osaka")
+		end,
+	})
 
-    -- Visualize indentation line
-    use {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("ibl").setup()
-        end
-    }
+	-- Visualize indentation line
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("ibl").setup()
+		end,
+	})
 
-    -- Track coding activity
-    use {
-        'wakatime/vim-wakatime'
-    }
+	-- Track coding activity
+	use({
+		"wakatime/vim-wakatime",
+	})
 
-    -- Built-in git
-    use {
-        'tpope/vim-fugitive'
-    }
+	-- Built-in git
+	use({
+		"tpope/vim-fugitive",
+	})
 
-    -- Find/grep mechanism
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	-- Find/grep mechanism
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    -- Undo/redo mechanism
-    use {
-        'mbbill/undotree'
-    }
+	-- Undo/redo mechanism
+	use({
+		"mbbill/undotree",
+	})
 
-    -- File switching shortcuts
-    use {
-        'theprimeagen/harpoon'
-    }
+	-- File switching shortcuts
+	use({
+		"theprimeagen/harpoon",
+	})
 
-    -- Language parser
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        tag = 'v0.10.0',
-        run = ':TSUpdate'
-    }
+	-- Language parser
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		tag = "v0.10.0",
+		run = ":TSUpdate",
+	})
 
-    -- LSP shenanigans
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v4.x'
-    }
-    use {
-        'neovim/nvim-lspconfig'
-    }
-    use {
-        'hrsh7th/nvim-cmp'
-    }
-    use {
-        'hrsh7th/cmp-nvim-lsp'
-    }
+	-- LSP shenanigans
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v4.x",
+	})
+	use({
+		"neovim/nvim-lspconfig",
+	})
+	use({
+		"hrsh7th/nvim-cmp",
+	})
+	use({
+		"hrsh7th/cmp-nvim-lsp",
+	})
 
-    -- Formatter
-    use {
-        "stevearc/conform.nvim"
-    }
+	-- Formatter
+	use({
+		"stevearc/conform.nvim",
+	})
 
-    -- Comment/uncomment mechanism
-    use {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup({
-                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-            })
-        end
-    }
-    use {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        config = function()
-            require('ts_context_commentstring').setup()
-        end
-    }
+	-- Comment/uncomment mechanism
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	})
+	use({
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("ts_context_commentstring").setup()
+		end,
+	})
 
-    -- Auto create char pair
-    use {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = function()
-            require("nvim-autopairs").setup()
-        end
-    }
+	-- Auto create char pair
+	use({
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	})
 
-    -- Wrap blocks with brackets/ticks
-    use {
-        "kylechui/nvim-surround",
-        tag = "*",
-        config = function()
-            require("nvim-surround").setup()
-        end
-    }
+	-- Wrap blocks with brackets/ticks
+	use({
+		"kylechui/nvim-surround",
+		tag = "*",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	})
 end)
